27f050c88009df59d1fce0af6ebb5bf31203b9e5
Vittorio <90337009+vittoopugliese@users.noreply.github.com>
2022-05-13 17:51:43 -0300

    type file problems

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const audioHat = document.getElementById("audioHat");
 document.addEventListener("keydown", sound);
 document.addEventListener("keyup", noSound);
 
-// sound functions
+// source functions
 const kick1 = document.getElementById("kick1");
 const kick2 = document.getElementById("kick2");
 const kick3 = document.getElementById("kick3");
@@ -37,23 +37,23 @@ let changeKick = 1;
 let changeClap = 1;
 let changeHat = 1;
 
-kick1.addEventListener("mouseover", () => (changeKick = 1));
-kick2.addEventListener("mouseover", () => (changeKick = 2));
-kick3.addEventListener("mouseover", () => (changeKick = 3));
-kick4.addEventListener("mouseover", () => (changeKick = 4));
-kick5.addEventListener("mouseover", () => (changeKick = 5));
+kick1.addEventListener("click", () => (changeKick = 1));
+kick2.addEventListener("click", () => (changeKick = 2));
+kick3.addEventListener("click", () => (changeKick = 3));
+kick4.addEventListener("click", () => (changeKick = 4));
+kick5.addEventListener("click", () => (changeKick = 5));
 
-clap1.addEventListener("mouseover", () => (changeClap = 1));
-clap2.addEventListener("mouseover", () => (changeClap = 2));
-clap3.addEventListener("mouseover", () => (changeClap = 3));
-clap4.addEventListener("mouseover", () => (changeClap = 4));
-clap5.addEventListener("mouseover", () => (changeClap = 5));
+clap1.addEventListener("click", () => (changeClap = 1));
+clap2.addEventListener("click", () => (changeClap = 2));
+clap3.addEventListener("click", () => (changeClap = 3));
+clap4.addEventListener("click", () => (changeClap = 4));
+clap5.addEventListener("click", () => (changeClap = 5));
 
-hat1.addEventListener("mouseover", () => (changeHat = 1));
-hat2.addEventListener("mouseover", () => (changeHat = 2));
-hat3.addEventListener("mouseover", () => (changeHat = 3));
-hat4.addEventListener("mouseover", () => (changeHat = 4));
-hat5.addEventListener("mouseover", () => (changeHat = 5));
+hat1.addEventListener("click", () => (changeHat = 1));
+hat2.addEventListener("click", () => (changeHat = 2));
+hat3.addEventListener("click", () => (changeHat = 3));
+hat4.addEventListener("click", () => (changeHat = 4));
+hat5.addEventListener("click", () => (changeHat = 5));
 
 const uploadKick = document.getElementById("uploadKick");
 const uploadClap = document.getElementById("uploadClap");
@@ -64,30 +64,40 @@ uploadKick.addEventListener("change", (e) => {
   console.log(kick);
 });
 
+function kickSound() {
+  audioKick.src = `files/kick${changeKick}.wav`;
+  audioKick.play();
+}
+function clapSound() {
+  audioClap.src = `files/clap${changeClap}.wav`;
+  audioClap.play();
+}
+function hatSound() {
+  audioHat.src = `files/hat${changeHat}.wav`;
+  audioHat.play();
+}
+
+// sound triggers when touch
+kick.addEventListener("click", kickSound);
+clap.addEventListener("click", clapSound);
+hat.addEventListener("click", hatSound);
+
+// to sound fns
 function sound(e) {
   if (e.repeat) return;
-
   if (e.key == kickLetter.innerHTML.toLowerCase()) {
-    audioKick.src = `files/kick${changeKick}.wav`;
-    audioKick.play();
-
+    kickSound();
     kick.style.backgroundColor = "rgb(22, 22, 22)";
   }
-
   if (e.key == clapLetter.innerHTML.toLowerCase()) {
-    audioClap.src = `files/clap${changeClap}.wav`;
-    audioClap.play();
-
+    clapSound();
     clap.style.backgroundColor = "rgb(64, 64, 64)";
   }
   if (e.key == hatLetter.innerHTML.toLowerCase()) {
-    audioHat.src = `files/hat${changeHat}.wav`;
-    audioHat.play();
-
+    hatSound();
     hat.style.backgroundColor = "rgb(130, 130, 130)";
   }
 }
-
 function noSound(e) {
   if (e.key == kickLetter.innerHTML.toLowerCase()) {
     kick.style.backgroundColor = "rgb(40, 40, 40)";
@@ -133,6 +143,7 @@ function hatKeydown(e) {
   hatLetter.innerHTML = `${e.key.toUpperCase()}`;
 }
 
+// styles :p
 const dw = document.querySelectorAll(".dropdown-content");
 const ddAct = document.querySelectorAll("#ddAct");
 

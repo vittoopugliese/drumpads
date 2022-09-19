cd314e07a6680e50fad67bb7a48e2f8e35c4cdeb
Vittorio <vittoopugliese@gmail.com>
2022-09-18 21:11:47 -0300

    finalized

--- app.js
+++ app.js
@@ -19,19 +19,16 @@ const kick1 = document.getElementById("kick1");
 const kick2 = document.getElementById("kick2");
 const kick3 = document.getElementById("kick3");
 const kick4 = document.getElementById("kick4");
-const kick5 = document.getElementById("kick5");
 
 const clap1 = document.getElementById("clap1");
 const clap2 = document.getElementById("clap2");
 const clap3 = document.getElementById("clap3");
 const clap4 = document.getElementById("clap4");
-const clap5 = document.getElementById("clap5");
 
 const hat1 = document.getElementById("hat1");
 const hat2 = document.getElementById("hat2");
 const hat3 = document.getElementById("hat3");
 const hat4 = document.getElementById("hat4");
-const hat5 = document.getElementById("hat5");
 
 let changeKick = 1;
 let changeClap = 1;
@@ -41,39 +38,90 @@ kick1.addEventListener("click", () => (changeKick = 1));
 kick2.addEventListener("click", () => (changeKick = 2));
 kick3.addEventListener("click", () => (changeKick = 3));
 kick4.addEventListener("click", () => (changeKick = 4));
-kick5.addEventListener("click", () => (changeKick = 5));
 
 clap1.addEventListener("click", () => (changeClap = 1));
 clap2.addEventListener("click", () => (changeClap = 2));
 clap3.addEventListener("click", () => (changeClap = 3));
 clap4.addEventListener("click", () => (changeClap = 4));
-clap5.addEventListener("click", () => (changeClap = 5));
 
 hat1.addEventListener("click", () => (changeHat = 1));
 hat2.addEventListener("click", () => (changeHat = 2));
 hat3.addEventListener("click", () => (changeHat = 3));
 hat4.addEventListener("click", () => (changeHat = 4));
-hat5.addEventListener("click", () => (changeHat = 5));
 
 const uploadKick = document.getElementById("uploadKick");
 const uploadClap = document.getElementById("uploadClap");
 const uploadHat = document.getElementById("uploadHat");
 
-uploadKick.addEventListener("change", (e) => {
-  let kick = e.target.files[0];
-  console.log(kick);
+let uploadedKick;
+let uploadedClap;
+let uploadedHat;
+
+uploadKick.addEventListener("change", (sample) => {
+  getKick(sample);
+  changeKick = 4;
+});
+uploadClap.addEventListener("change", (sample) => {
+  getClap(sample);
+  changeClap = 4;
+});
+uploadHat.addEventListener("change", (sample) => {
+  getHat(sample);
+  changeHat = 4;
 });
 
+function getKick(sample) {
+  let file = sample.target.files[0];
+  let reader = new FileReader();
+
+  reader.readAsDataURL(file);
+  reader.onload = () => {
+    uploadedKick = reader.result.toString();
+  };
+}
+
+function getClap(sample) {
+  let file = sample.target.files[0];
+  let reader = new FileReader();
+
+  reader.readAsDataURL(file);
+  reader.onload = () => {
+    uploadedClap = reader.result.toString();
+  };
+}
+
+function getHat(sample) {
+  let file = sample.target.files[0];
+  let reader = new FileReader();
+
+  reader.readAsDataURL(file);
+  reader.onload = () => {
+    uploadedHat = reader.result.toString();
+  };
+}
+
 function kickSound() {
-  audioKick.src = `files/kick${changeKick}.wav`;
+  if (changeKick == 4) {
+    audioKick.src = uploadedKick;
+  } else {
+    audioKick.src = `files/kick${changeKick}.wav`;
+  }
   audioKick.play();
 }
 function clapSound() {
-  audioClap.src = `files/clap${changeClap}.wav`;
+  if (changeClap == 4) {
+    audioClap.src = uploadedClap;
+  } else {
+    audioClap.src = `files/clap${changeClap}.wav`;
+  }
   audioClap.play();
 }
 function hatSound() {
-  audioHat.src = `files/hat${changeHat}.wav`;
+  if (changeHat == 4) {
+    audioHat.src = uploadedHat;
+  } else {
+    audioHat.src = `files/hat${changeHat}.wav`;
+  }
   audioHat.play();
 }
 
@@ -154,3 +202,28 @@ dw[2].style.backgroundColor = "rgb(144, 144, 144)";
 ddAct[0].style.backgroundColor = "rgb(40, 40, 40)";
 ddAct[1].style.backgroundColor = "rgb(78, 78, 78)";
 ddAct[2].style.backgroundColor = "rgb(144, 144, 144)";
+
+// extra
+
+const userGuide = document.querySelector("#userGuide");
+const modal = document.querySelector(".modal");
+const modalBg = document.querySelector(".modalBg");
+userGuide.addEventListener("click", () => {
+  modal.style.display = "flex";
+  modalBg.style.display = "flex";
+  setTimeout(() => {
+    modal.style.opacity = "1";
+    modalBg.style.opacity = "1";
+  }, 154);
+});
+
+modalBg.addEventListener("click", closeModal);
+
+function closeModal() {
+  modal.style.opacity = "0";
+  modalBg.style.opacity = "0";
+  setTimeout(() => {
+    modal.style.display = "none";
+    modalBg.style.display = "none";
+  }, 200);
+}
